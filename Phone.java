ae53127c6f321fe80145db041f4b7c4dff63b9ca
Tiyarong Gurung <dipeshtamu95@gmail.com>
2022-08-08 23:26:31 -0400

    completed

---
+++ Phone.java
@@ -0,0 +1,27 @@
+public abstract class Phone {
+	private String versionNumber;
+	private int batteryPercentage;
+	private String carrier;
+    private String ringTone;
+    public String getRingTone() {
+        return this.ringTone;
+    }
+    public int getBatteryPercentage() {
+        return this.batteryPercentage;
+    }
+    public String getCarrier() {
+        return this.carrier;
+    }
+    public String getVersionNumber() {
+        return this.versionNumber;
+    }
+
+    public Phone(String versionNumber, int batteryPercentage, String carrier, String ringTone){
+        this.versionNumber = versionNumber;
+        this.batteryPercentage = batteryPercentage;
+        this.carrier = carrier;
+        this.ringTone = ringTone;
+    }
+    public abstract void displayInfo();
+    
+}
